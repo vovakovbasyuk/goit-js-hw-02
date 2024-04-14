10ed9c78116401fefdc5ca737fdeb409916df976
vovakovbasyuk <vova.kovbasyuk.04@gmail.com>
2024-04-14 11:14:00 +0300

    hm

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,14 @@
-function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = price + deliveryFee;
-  return `Shipping to ${country} will cost ${totalPrice} credits`;
+function formatMessage(message, maxLength) {
+  const strLength = message.length;
+  if (strLength <= maxLength) {
+    return message;
+  } else {
+    return message.slice(0, maxLength) + '...';
+  }
 }
-
-console.log(getShippingMessage('Australia', 120, 50));
-console.log(getShippingMessage('Germany', 80, 20));
-console.log(getShippingMessage('Sweden', 100, 20));
+console.log(formatMessage('Curabitur ligula sapien', 16));
+console.log(formatMessage('Curabitur ligula sapien', 23));
+console.log(formatMessage('Vestibulum facilisis purus nec', 20));
+console.log(formatMessage('Vestibulum facilisis purus nec', 30));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 15));
+console.log(formatMessage('Nunc sed turpis a felis in nunc fringilla', 41));
--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,16 @@
-function getElementWidth(content, padding, border) {
-  const content1 = Number.parseFloat(content);
-  const padding1 = Number.parseFloat(padding);
-  const border1 = Number.parseFloat(border);
-  const totalWidth = content1 + padding1 * 2 + border1 * 2;
-
-  return totalWidth;
+function checkForSpam(message) {
+  const lowStr = message.toLowerCase();
+  const spamWords = lowStr.includes('spam') || lowStr.includes('sale');
+  if (!spamWords) {
+    return false;
+  } else {
+    return true;
+  }
 }
-
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
